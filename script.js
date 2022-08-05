dc944fb04ed9f2a50e35a70edc80a331c7e61684
Usama <usamamughal4949@gmail.com>
2022-08-06 00:02:14 +0500

    using multiple selections and adding a class

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 $(document).ready(function (){
-    //changing container text
-    $("#container").html("<b>hello world</b>")
-    //
-    $("li").text("hello")
+    // //changing container html
+    // $("#container").html("<b>hello world</b>")
+    // //changing list items text
+    // $("li").text("hello")
+    $('.class2,.class3').addClass('highlight')
 });
