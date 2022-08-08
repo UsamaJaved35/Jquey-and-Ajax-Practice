b0d0dd5b8fca4387fd8ae8d8b6e0d33ca76ce520
Usama <usamamughal4949@gmail.com>
2022-08-09 01:09:47 +0500

    parent travesing

--- script.js
+++ script.js
@@ -32,23 +32,37 @@
 
 /*LESSON 5- DOM TRAVERSING */
 
+// $(document).ready(function(){
+// //    //using find method
+// //     var result=$('#animals').find('.creature');
+// //    console.log(result) 
+//     //using children method for direct descendents
+//     // var result=$('#animals').children('.creature')
+//     // console.log(result)
+//     // using first(),last(),prev(),next()
+//     //using first()
+//     //var res=$('#animals').children().children().first()
+//     //console.log(res)
+//     //using last()
+//     //var res=$('#animals').children().last().first()
+//     //console.log(res)
+//     //using next() and prev()
+//     // var res=$('#animals').children().first().children().first().next()
+//     // console.log(res)
+//     // res=$('#animals').children().first().children().last().prev().prev()
+//     // console.log(res)
+// });
+
+/*LESSON 6 - TRAVERSING PARENT ELEMENTS */
+
 $(document).ready(function(){
-//    //using find method
-//     var result=$('#animals').find('.creature');
-//    console.log(result) 
-    //using children method for direct descendents
-    // var result=$('#animals').children('.creature')
-    // console.log(result)
-    // using first(),last(),prev(),next()
-    //using first()
-    //var res=$('#animals').children().children().first()
-    //console.log(res)
-    //using last()
-    //var res=$('#animals').children().last().first()
-    //console.log(res)
-    //using next() and prev()
-    var res=$('#animals').children().first().children().first().next()
-    console.log(res)
-    res=$('#animals').children().first().children().last().prev().prev()
-    console.log(res)
+    //using parents
+   var result=$('#cat').parent();
+   console.log(result) 
+    // using parents
+    result=$('#cat').parents();
+    console.log(result)
+    //using closest
+    result=$('#cat').closest('.category');
+    console.log(result)
 });
